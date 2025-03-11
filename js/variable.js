48801aec78c62cc92445dae0ad2cf75b8912e149
kawa0622 <fmhcf972@yahoo.co.jp>
2025-03-11 17:30:42 +0900

    4.4 変数の中身を入れ替えてみよう

--- js/variable.js
+++ js/variable.js
@@ -10,3 +10,11 @@ let userNumber = 55;
 //コンソールへの出力
 console.log(userName);
 console.log(userNumber);
+
+//値の再代入
+userName = '侍花子';
+userNumber = 88;
+
+//コンソールへの出力
+console.log(userName);
+console.log(userNumber);
