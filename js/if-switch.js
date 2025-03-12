a59fbec322951e35a90f356d91ed660a12ad52b1
kawa0622 <fmhcf972@yahoo.co.jp>
2025-03-12 20:24:55 +0900

    8.4 switch文を書いてみよう

--- js/if-switch.js
+++ js/if-switch.js
@@ -25,20 +25,27 @@ else if (num===3) {
   console.log('当たりです');
 }
 //それ以外のときは、「はずれです」という文字列を出力する
-else {console.log('はずれです');}
+else {console.log('はずれです');
 
-num = 20;
-
-//&&（かつ）を記述して、全ての条件が成り立つ場合にのみ処理を行う（変数numの値が11～29）
-if (num > 10 && num < 30) {
-  console.log('変数numは10より大きく、30より小さいです');
 }
-else {console.log('条件が成り立ちませんでした');}
 
-  num = 20;
+//変数numに0～4までのランダムな整数を代入する
+num = Math.floor(Math.random() * 5);
+//変数numの値を出力する
+console.log(num);
 
-  //||（または）を記述して、１つでも条件が成り立てば処理を行う（変数numの値が10または30）
-  if (num ===10 || num ===30) {
-    console.log('変数numは10または30です');
-  } else {console.log('条件が成り立ちませんでした');
+//変数numの値によって、出力する文字列を切り替える
+switch (num) {
+  case 2:
+    console.log('小吉です');
+    break;
+    case 3:
+      console.log('中吉です');
+    break;
+  case 4:
+    console.log('大吉です');
+    break;
+default:
+  console.log('末吉です');
+  break;
 }
