3faec4e78755d503a95539dd08d1bbbf39799850
kawa0622 <fmhcf972@yahoo.co.jp>
2025-03-20 18:25:00 +0900

    querySelector()：HTML要素をCSSセレクタで取得する（最初の1つ）

--- js/dom.js
+++ js/dom.js
@@ -19,3 +19,8 @@ const headings = document.getElementsByClassName('heading');
 for (let i = 0; i < headings.length; i++) {
   console.log(headings[i]);
 }
+
+//HTML要素をCSSセレクタで取得し、中身を出力する（最初の一つ）
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#second-heading'));
+console.log(document.querySelector('.list'));
